986699d6e8bcd4c703c7a7689951ecb074b9e695
lee000403 <dong000403@gmail.com>
2023-08-01 18:22:24 +0900

    commit

---
+++ datatype_list.py
@@ -0,0 +1,4 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+thislist[1:3]
+thislist[1] = "watermelon"
+pass
--- datatype_strings.py
+++ datatype_strings.py
@@ -1,3 +1,16 @@
-str = "yojulab !"
-len(str)
+str4 = "yojulab !"
+str1 = 1
+str2 = 3.0
+
+print("{}입니다".format(str2))
+
+age = 36
+txt = "My name is John, I am " + str(age)
+print(txt)
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {0} pieces of item {1} for {2} dollars. I have just {2}."
+
 pass
